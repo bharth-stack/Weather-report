3119d7d1fa24523adba8147ee51a07cf52d0907c
Bharath <63013663928b@gmail.com>
2021-06-29 21:07:42 +0530

    Weather report

---
+++ script.js
@@ -0,0 +1,118 @@
+let weather = {
+  apiKey: "745e82d350b49f130544c2400e8eb24e",
+  fetchWeather: function (city) {
+    fetch(
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+        city +
+        "&units=metric&appid=" +
+        this.apiKey
+    )
+      .then((response) => {
+        if (!response.ok) {
+          alert("No weather found.");
+          throw new Error("No weather found.");
+        }
+        return response.json();
+      })
+      .then((data) => this.displayWeather(data));
+  },
+  displayWeather: function (data) {
+    const { name } = data;
+    const { icon, description } = data.weather[0];
+    const { temp, humidity } = data.main;
+    const { speed } = data.wind;
+    document.querySelector(".city").innerText = "Weather in " + name;
+    document.querySelector(".icon").src =
+      "https://openweathermap.org/img/wn/" + icon + ".png";
+    document.querySelector(".description").innerText = description;
+    document.querySelector(".temp").innerText = temp + "°C";
+    document.querySelector(".humidity").innerText =
+      "Humidity: " + humidity + "%";
+    document.querySelector(".wind").innerText =
+      "Wind speed: " + speed + " km/h";
+    document.querySelector(".weather").classList.remove("loading");
+    document.body.style.backgroundImage =
+      "url('https://source.unsplash.com/1600x900/?" + name + "')";
+  },
+  search: function () {
+    this.fetchWeather(document.querySelector(".search-bar").value);
+  },
+};
+
+document.querySelector(".search button").addEventListener("click", function () {
+  weather.search();
+});
+
+document
+  .querySelector(".search-bar")
+  .addEventListener("keyup", function (event) {
+    if (event.key == "Enter") {
+      weather.search();
+    }
+  });
+
+function myFunction() {
+  document.getElementById("myDropdown").classList.toggle("show");
+}
+
+// Close the dropdown if the user clicks outside of it
+window.onclick = function (event) {
+  if (!event.target.matches(".dropbtn")) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    var i;
+    for (i = 0; i < dropdowns.length; i++) {
+      var openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
+      }
+    }
+  }
+};
+setInterval(setClock, 1000);
+
+const hourHand = document.querySelector("[data-hour-hand]");
+const minuteHand = document.querySelector("[data-minute-hand]");
+const secondHand = document.querySelector("[data-second-hand]");
+
+function setClock() {
+  const currentDate = new Date();
+  const secondsRatio = currentDate.getSeconds() / 60;
+  const minutesRatio = (secondsRatio + currentDate.getMinutes()) / 60;
+  const hoursRatio = (minutesRatio + currentDate.getHours()) / 12;
+  setRotation(secondHand, secondsRatio);
+  setRotation(minuteHand, minutesRatio);
+  setRotation(hourHand, hoursRatio);
+}
+setClock();
+
+function setRotation(element, rotationRatio) {
+  element.style.setProperty("--rotation", rotationRatio * 360);
+}
+
+let colorChanger = function (color, colors) {
+  document.querySelector(".card").style.backgroundColor = colors[color];
+  document.querySelector(".dropbtn").style.backgroundColor = colors[color];
+  document.querySelector(".search-bar").style.backgroundColor = "#0c0b0b7a";
+  document.querySelector(".card-btn").style.backgroundColor = "#0c0b0b7a";
+  document.querySelector(".clock").style.backgroundColor = colors[color];
+
+  document.querySelector(".dropdown-content").style.backgroundColor =
+    colors[color];
+};
+
+let color = document.querySelectorAll(".color");
+let colors = [
+  "rgba(102, 48, 104, 0.616)",
+  "rgba(53, 104, 48, 0.616)",
+  "rgba(126, 62, 53, 0.849)",
+];
+
+color[0].addEventListener("click", function () {
+  colorChanger(0, colors);
+});
+color[1].addEventListener("click", function () {
+  colorChanger(1, colors);
+});
+color[2].addEventListener("click", function () {
+  colorChanger(2, colors);
+});
